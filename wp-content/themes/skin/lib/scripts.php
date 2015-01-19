661fc37b73b9f8518a00a40ace52d4d88a47bedb
Grégory Copin <gregcop1@gmail.com>
2015-01-20 00:10:57 +0100

    [add] mainMenu

--- wp-content/themes/skin/lib/scripts.php
+++ wp-content/themes/skin/lib/scripts.php
@@ -20,7 +20,7 @@
    */
   function get_local_footer() {
     if(preg_match('/local\.com/', $_SERVER['HTTP_HOST'])) {
-      echo '<script type="text/javascript" id="__bs_script__"> document.write("<script async src=\'//HOST:3000/browser-sync/browser-sync-client.1.8.2.js\'><\/script>".replace(/HOST/g, location.hostname).replace(/PORT/g, location.port)); </script> ';
+      echo '<script type="text/javascript" id="__bs_script__"> document.write("<script async src=\'//HOST:3000/browser-sync/browser-sync-client.1.9.0.js\'><\/script>".replace(/HOST/g, location.hostname).replace(/PORT/g, location.port)); </script> ';
     }
   }
 
